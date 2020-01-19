fc76a31142e124b38816f5351cac081e3acc4624
sinuhe Montero <sinuhem23@gmail.com>
2020-01-18 20:38:23 -0500

    testing

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './home.css';
+
+function App() {
+  return (
+    <div>
+      <div className='backgroundImg'>
+        <p className='titleEm'>Essentials By Em</p>
+        <div className='container'>
+          <p>Hello!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
